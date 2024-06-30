d73913534b1cad3bea77fc0e8a5a67dfa2197bfc
buchi-1996 <itzbuchi@gmail.com>
2024-06-30 13:19:04 +0100

    Added sidebar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,29 @@
 gsap.registerPlugin(ScrollTrigger)
+const navbarOverlay = document.querySelector('.nav_sidebar_overlay');
+const navSidebar = document.querySelector('.nav_sidebar');
+const menuButton = document.querySelector('.menu')
+const closeButton = document.querySelector('.close-btn')
+
+
+
+const handleOpenMenu= () => {
+    navbarOverlay.classList.add('show_sidebar')
+    navSidebar.classList.add('show_sidebar')
+}
+
+const handleCloseMenu = () => {
+    navbarOverlay.classList.remove('show_sidebar')
+    navSidebar.classList.remove('show_sidebar')
+}
+
+const closeOnOutClick = () => {
+    navbarOverlay.classList.remove('show_sidebar')
+    navSidebar.classList.remove('show_sidebar')
+}
+
+menuButton.addEventListener('click', handleOpenMenu);
+closeButton.addEventListener('click', handleCloseMenu);
+navbarOverlay.addEventListener('click', closeOnOutClick);
 
 gsap.utils.toArray('.product').forEach(product => {
     ScrollTrigger.create({
@@ -12,19 +37,21 @@ gsap.utils.toArray('.product').forEach(product => {
 document.addEventListener('DOMContentLoaded', function() {
     const header = document.querySelector('.header');
     const headerLinks = document.querySelectorAll('.header a');
+    
   
     ScrollTrigger.create({
       start: "top top",
       end: 99999,
       onUpdate: self => {
         if (window.scrollY === 0) {
-            gsap.to(header, { backgroundColor: "transparent", color: "white", duration: 0.3, borderBottom: 'none' });
+            gsap.to(header, { backgroundColor: "transparent",  color: "white", duration: 0.3, borderBottom: 'none' });
             gsap.to(headerLinks, { color: "white", duration: 0.3 });
           } else {
-            gsap.to(header, { backgroundColor: "rgba(0, 0, 0, 0.8)", color: "white", duration: 0.3, borderBottom: "1px solid #555" });
+            gsap.to(header, { backgroundColor: "#000",  color: "white", duration: 0.3, borderBottom: "1px solid #555" });
             gsap.to(headerLinks, { color: "white", duration: 0.3 });
           }
       }
     });
   });
+
 
