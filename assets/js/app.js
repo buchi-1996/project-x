2566b8fdfb8f914c04b9745780394e377ca428e0
buchi-1996 <itzbuchi@gmail.com>
2024-07-04 16:49:58 +0100

    Added sidebar contents

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger, Draggable)
 const navbarOverlay = document.querySelector('.nav_sidebar_overlay');
 const navSidebar = document.querySelector('.nav_sidebar');
 const menuButton = document.querySelector('.menu')
@@ -53,5 +53,113 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     });
   });
+
+//   let slideDelay = 1.4;
+// let slideDuration = 0.8;
+// let wrap = true;
+
+// let slides = document.querySelectorAll(".slide");
+// let prevButton = document.querySelector("#prevButton");
+// let nextButton = document.querySelector("#nextButton");
+// let progressWrap = gsap.utils.wrap(0, 1);
+
+// let numSlides = slides.length;
+
+// // pp
+
+// let wrapX = gsap.utils.wrap(-100, (numSlides - 1) * 100);
+// let timer = gsap.delayedCall(slideDelay, autoPlay);
+
+// let animation = gsap.to(slides, {
+//   xPercent: "+=" + (numSlides * 100),
+//   duration: 1,
+//   ease: "none",
+//   paused: true,
+//   repeat: -1,
+//   modifiers: {
+//     xPercent: wrapX
+//   }
+// });
+
+// let proxy = document.createElement("div");
+// let slideAnimation = gsap.to({}, {});
+// let slideWidth = 0;
+// let wrapWidth = 0;
+
+// let draggable = new Draggable(proxy, {
+//   trigger: ".slides-container",
+//   inertia: true,
+//   onPress: updateDraggable,
+//   onDrag: updateProgress,
+//   onThrowUpdate: updateProgress,
+//   snap: {     
+//     x: snapX
+//   }
+// });
+
+// resize();
+
+// window.addEventListener("resize", resize);
+
+// prevButton.addEventListener("click", function() {
+//   animateSlides(1);
+// });
+
+// nextButton.addEventListener("click", function() {
+//   animateSlides(-1);
+// });
+
+// function updateDraggable() {
+//   timer.restart(true);
+//   slideAnimation.kill();
+//   this.update();
+// }
+
+// function animateSlides(direction) {
+    
+//   timer.restart(true);
+//   slideAnimation.kill();
+//   let x = snapX(gsap.getProperty(proxy, "x") + direction * slideWidth);
+  
+//   slideAnimation = gsap.to(proxy, {
+//     x: x,
+//     duration: slideDuration,
+//     onUpdate: updateProgress
+//   });  
+// }
+
+// function autoPlay() {  
+// //   if (draggable.isPressed || draggable.isDragging || draggable.isThrowing) {
+// //     timer.restart(true);
+// //   } else {
+//     animateSlides(1);
+
+// }
+
+// function updateProgress() { 
+//   animation.progress(progressWrap(gsap.getProperty(proxy, "x") / wrapWidth));
+// }
+
+// function snapX(value) {
+//   let snapped = gsap.utils.snap(slideWidth, value);
+//   return wrap ? snapped : gsap.utils.clamp(-slideWidth * (numSlides - 1), 0, snapped);
+// }
+
+// function resize() {
+  
+//   let norm = (gsap.getProperty(proxy, "x") / wrapWidth) || 0;
+  
+//   slideWidth = slides[0].offsetWidth;
+//   wrapWidth = slideWidth * numSlides;
+  
+//   wrap || draggable.applyBounds({minX: -slideWidth * (numSlides - 1), maxX: 0});
+  
+//   gsap.set(proxy, {
+//     x: norm * wrapWidth
+//   });
+  
+//   animateSlides(0);
+//   slideAnimation.progress(1);
+// }
 
 
---
+++ assets/js/configurator/conf.js
@@ -0,0 +1,163 @@
+class SideBarActions {
+    constructor() {
+        this.sidebarButtons = document.querySelectorAll('.sidebar_menu-item')
+        this.sidebarSettingsModal = document.querySelector('.tools_sidebar')
+        this.sidebarModalContents = document.querySelector('.tools_sidebar-content')
+        this.closeSettingsModalBtn = document.querySelector('.settings-close-btn')
+        this.settingsTitle;
+        this.currentMenuItem;
+        this.handleSidebarButtonClick()
+        this.showSettingsModal('Door Frame', 0)
+        this.closeSettingsModalBtn.addEventListener('click', this.closeSettingsModal)
+    }
+
+    handleSidebarButtonClick = () => {
+        this.sidebarButtons.forEach(button => {
+            button.addEventListener('click', (e) => {
+                console.log(button);
+                this.closeSettingsModal();
+                this.settingsTitle = button.children[0].lastElementChild.textContent
+                this.currentMenuItem = +button.dataset.id
+                setTimeout(() => {
+                    this.showSettingsModal(this.settingsTitle, this.currentMenuItem)
+                }, 40);
+            })
+        })
+    }
+
+    // Sidebar Menu options
+    doorFrames = () => {
+        return(
+            `<ul class="door-frames">
+                        <li class="door-frame">
+                            <span class="selected">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                                  </svg>                                  
+                            </span>
+                            <img src="./assets/images/frames/frame-sample-1.png" width="400px" alt="">
+                            <small>frame 1</small>
+                        </li>
+                        <li class="door-frame">
+                            <img src="./assets/images/frames/frame-sample-2.png" alt="">
+                            <small>frame 2</small>
+                        </li>
+                        <li class="door-frame">
+                            <img src="./assets/images/frames/frame-sample-3.png" alt="">
+                            <small>frame 3</small>
+                        </li>
+                        <li class="door-frame">
+                            <img src="./assets/images/frames/frame-sample-4.png" alt="">
+                            <small>frame 4</small>
+                        </li>
+                        <li class="door-frame">
+                            <img src="./assets/images/frames/frame-sample-5.png" alt="">
+                            <small>frame 5</small>
+                        </li>
+                        <li class="door-frame">
+                            <img src="./assets/images/frames/frame-sample-6.png" alt="">
+                            <small>frame 6</small>
+                        </li>
+                        <li class="door-frame">
+                            <img src="./assets/images/frames/frame-sample-7.png" alt="">
+                            <small>frame 7</small>
+                        </li>
+                        <li class="door-frame">
+                            <img src="./assets/images/frames/frame-sample-8.png" alt="">
+                            <small>frame 8</small>
+                        </li>
+                        <li class="door-frame">
+                            <img src="./assets/images/frames/frame-sample-9.png" alt="">
+                            <small>frame 9</small>
+                        </li>
+                        <li class="door-frame">
+                            <img src="./assets/images/frames/frame-sample-10.png" alt="">
+                            <small>frame 10</small>
+                        </li>
+                        <li class="door-frame">
+                            <img src="./assets/images/frames/frame-sample-11.png" alt="">
+                            <small>frame 11</small>
+                        </li>
+                     </ul>
+            `
+        )
+    }
+
+    housefront = () => {
+        return (
+            `<!-- Upload House Front -->
+                    <form>
+                        <label for="house-sample">
+                            <span>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z" />
+                                </svg>
+                            </span>
+                            Upload House Front
+                            <input type="file" id="house-sample" name="house-sample">
+                        </label>
+                    </form>
+                    <!-- House fronts -->
+                    <ul class="house-fronts">
+                        <li class="house-front">
+                        <span class="selected">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                                  </svg>                                  
+                            </span>
+                            <img src="./assets/images/house-front/c53a5bf9-d20c-441c-840b-114e267968cf.png" alt="">
+                        </li>
+                        <li class="house-front">
+                            <img src="./assets/images/house-front/c5a1d5c1-4257-4c53-bbef-e975ca66ebf9.png" alt="">
+                        </li>
+                        <li class="house-front">
+                            <img src="./assets/images/house-front/cc6b221e-9b3a-49eb-b8ec-6f4fec8c51c6.png" alt="">
+                        </li>
+                        <li class="house-front">
+                            <img src="./assets/images/house-front/0fe4cc53-f90c-4f07-89a8-a369814d5a6d.png" alt="">
+                        </li>
+                        <li class="house-front">
+                            <img src="./assets/images/house-front/3f2bd0a3-e074-4ce0-bfec-80d3d33d89b7.png" alt="">
+                        </li>
+                        <li class="house-front">
+                            <img src="./assets/images/house-front/6ca6e2eb-4a8b-4e7a-ba90-74386173fc0c.png" alt="">
+                        </li>
+                    </ul>`
+        )
+    }
+
+    showSettingsModal = (title, id) => {
+        // if title matches show settings for title
+        this.sidebarSettingsModal.scrollTop = 0;
+        this.sidebarSettingsModal.firstElementChild.firstElementChild.innerText = title
+        if (id === 0) {
+            this.sidebarModalContents.innerHTML = this.doorFrames()
+        }else if(id === 1){
+            this.sidebarModalContents.innerHTML = this.housefront()
+        }else if(id === 2){
+            this.sidebarModalContents.innerHTML = ''
+        }else if(id === 3){
+            this.sidebarModalContents.innerHTML = ''
+        }else if(id === 4){
+            this.sidebarModalContents.innerHTML = ''
+        }else if(id === 5){
+            this.sidebarModalContents.innerHTML = ''
+        }else if(id === 6){
+            this.sidebarModalContents.innerHTML = ''
+        }
+
+
+        this.sidebarSettingsModal.classList.add('showModal')
+    }
+
+    closeSettingsModal = () => {
+        this.sidebarSettingsModal.classList.remove('showModal')
+    }
+
+}
+
+let sidebarActions = new SideBarActions()
